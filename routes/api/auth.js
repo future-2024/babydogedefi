c347cd507b11c4b5d42c117846f87ba861257465
Roman Fedco <future55star@mail.ru>
2022-03-09 08:58:33 +0300

    getUser fixed.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -86,4 +86,15 @@ router.post(
   }
 );
 
+router.get('/getUser', async (req, res) => {
+  try {
+    const user = await User.find();
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
